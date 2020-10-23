09c9b56df288cd7e14787123fa61d4119df34696
akhmedruslanov <73357425+akhmedruslanov@users.noreply.github.com>
2020-10-23 20:05:09 +0300

    Add files via upload

---
+++ Lab2/TestAuthor.java
@@ -0,0 +1,9 @@
+﻿package gibo.01.18.akhmed;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1= new Author("Akhmed", "cristianoronaldo788.cr@gmail.com", 'M');
+        System.out.println(a1);
+        }
+    }
+
